da5847c29428d6d9ad565a957e72db8fd278dda9
Денис <pirrekd@mail.ru>
2026-03-23 11:18:08 +0300

    Доделал lesson_2

--- lesson_2/lesson2.js
+++ lesson_2/lesson2.js
@@ -8,70 +8,78 @@ const instructions = {
 };
 
 const program = [
-  instructions["SET A"],  // 0 - установить начальное значение
-  40,                     // 1
+  instructions["SET A"], // 0 - установить начальное значение
+  40, // 1
   instructions["PRINT A"], // 2 - вывести значение
-  instructions["DEC A"],   // 3 - уменьшить A на 1
-  instructions["IFN A"],   // 4 - если A не 0, то перейти на PRINT A
-  instructions["RET"],     // 5 - иначе завершиться
-  0,                       // 6 - код возврата
-  instructions["JMP"],     // 7 - переход на PRINT A (когда A != 0)
-  2,                       // 8 - индекс PRINT A
+  instructions["DEC A"], // 3 - уменьшить A на 1
+  instructions["IFN A"], // 4 - если A не 0, то перейти на PRINT A
+  instructions["RET"], // 5 - иначе завершиться
+  0, // 6 - код возврата
+  instructions["JMP"], // 7 - переход на PRINT A (когда A != 0)
+  2, // 8 - индекс PRINT A
 ];
 
 function execute(program) {
-  let acc;
-  let i = 0;
+    let memory = 0;
+    let pointer = 0;
 
-  while (true) {
-    const currentInst = program[i];
-    
-    switch (program[i]) {
-      case instructions["SET A"]: {
-        acc = program[i + 1];
-        i += 2;
-        break;
-      }
-      
-      case instructions["PRINT A"]: {
-        i++;
-        break;
-      }
-      
-      case instructions["IFN A"]: {
-        console.log(`  -> IFN A: A=${acc}`);
-        if (acc !== 0) {
-          i += 3; // пропуск условия
-        } else {
-          i++; // прямой переход на RET
+    const instructionLengths = {
+        0: 2, // SET A 
+        1: 1, // PRINT A
+        2: 1, // IFN A
+        3: 2, // RET 
+        4: 1, // DEC A
+        5: 2,  // JMP 
+    };
+
+    while (pointer < program.length) {
+        const opcode = program[pointer];
+
+        switch (opcode) {
+            case instructions['SET A']: 
+                memory = program[pointer + 1];
+                pointer += 2;
+                break;
+
+            case instructions['PRINT A']:
+                console.log(memory);
+                pointer += 1;
+                break;
+
+            case instructions['IFN A']: 
+                pointer += 1;
+
+                // Если A не равно 0, пропускаем 
+                if (memory !== 0) {
+
+                    const nextOpcode = program[pointer];
+                    const nextLength = instructionLengths[nextOpcode];
+
+                    pointer += nextLength;
+                }
+
+                break;
+
+            case instructions['RET']:
+                const retVal = program[pointer + 1];
+                return retVal;
+
+            case instructions['DEC A']: 
+                memory -= 1;
+                pointer += 1;
+                break;
+
+            case instructions['JMP']:
+                const addr = program[pointer + 1];
+                pointer = addr;
+                break;
+
+            default:
+                throw new Error(`Неизвестная команда: ${ opcode } в позиции ${ pointer }`);
         }
-        break;
-      }
-      
-      case instructions["RET"]: {
-        const returnCode = program[i + 1];
-        return returnCode;
-      }
-      
-      case instructions["DEC A"]: {
-        acc--;
-        i++;
-        break;
-      }
-      
-      case instructions["JMP"]: {
-        const jumpTo = program[i + 1];
-        i = jumpTo;
-        break;
-      }
-      
-      default: {
-        console.error(`Unknown instruction: ${currentInst} at index ${i}`);
-        return -1;
-      }
     }
-  }
 }
+
 
 const result = execute(program);
 console.log(`\n=== Программа завершилась с кодом: ${result} ===`);
